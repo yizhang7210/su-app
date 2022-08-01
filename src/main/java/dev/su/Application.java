d4611bb025cd7847eb0ca39704dd81ed4bfccb21
Yi Zhang <yi.zhang7210@gmail.com>
2022-07-31 21:07:38 -0700

    add spring boot

--- src/main/java/dev/su/Application.java
+++ src/main/java/dev/su/Application.java
@@ -1,11 +1,13 @@
 package dev.su;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new Application().getGreeting());
+        SpringApplication.run(Application.class, args);
     }
+
 }
